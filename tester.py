8d699959e3a8906d3a3ff14e77d9170f0cc788db
Jessica <jessicazhu@MacBook-Pro.local>
2022-11-20 15:45:59 -0500

    parsing genome fasta

--- tester.py
+++ tester.py
@@ -6,3 +6,11 @@ Created on Sun Nov 20 14:32:47 2022
 @author: jessicazhu
 """
 
+import numpy as np 
+from Bio import SeqIO
+
+seq_iterator = SeqIO.parse("genomic.fna.txt", "fasta")
+chromosome_1 = next(seq_iterator)
+#chromosome 1 sequence
+genome_seq = chromosome_1.seq
+print(genome_seq)
