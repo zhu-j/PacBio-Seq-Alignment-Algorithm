6480e7fedd01224a1e8865af0a6dc698a7515073
Jessica <jessicazhu@MacBook-Pro.local>
2022-11-21 19:02:55 -0500

    added code for blasting reads against genome seq

--- tester.py
+++ tester.py
@@ -4,60 +4,54 @@
 Created on Sun Nov 20 14:32:47 2022
 
 """
-
+"""
+Prequisite: install blast+ suite from https://anaconda.org/bioconda/blast
+"""
 import numpy as np
 import subprocess
 from io import StringIO
 from Bio.Blast import NCBIXML
+from itertools import islice, count
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIWWW
+import _pickle as pickle
 seq_iterator = SeqIO.parse("genomic.fna.txt", "fasta")
 chromosome_1 = next(seq_iterator)
+
 #chromosome 1 sequence
 genome_seq = chromosome_1.seq
-#print(genome_seq)
-read_1 = next(SeqIO.parse("readsMappingToChr1.fa.txt", "fasta"))
-read = SeqIO.parse("readsMappingToChr1.fa.txt", "fasta")
-all_read = []
-with open("example.fasta") as handle:
-    for record in read:
-        all_read.append(str(record.seq))
-# Create two sequence files
-seq1 = SeqRecord(Seq(read_1.seq),
-                   id="seq1")
-seq2 = SeqRecord(genome_seq,
-                   id="seq2")
-SeqIO.write(seq1, "seq1.fasta", "fasta")
-SeqIO.write(seq2, "seq2.fasta", "fasta")
+chr1_seq = SeqRecord(genome_seq,
+                 id="chr1_seq")
+SeqIO.write(chr1_seq, "chr1_seq.fasta", "fasta")
+dict_reads = {}
 
-# Run BLAST and parse the output as XML
-'''
-result_handle = NCBIWWW.qblast("blastn", "nt", read_1.seq)
-with open("my_blast.xml", "w") as out_handle:
- out_handle.write(result_handle.read())
+#blast individual reads against chromosome 1 subject seq
+#generate dictionary where key has format (index, read sequence) and value of (genome region start, genome region end)
+for index,read in enumerate(SeqIO.parse("readsMappingToChr1.fa.txt", "fasta")):
+    seq1 = SeqRecord(Seq(read.seq),
+                     id="seq1")
+    SeqIO.write(seq1, "seq1.fasta", "fasta")
+    (seq_s_e,blank) = NcbiblastnCommandline(query="seq1.fasta", subject="seq2.fasta", outfmt='10 sstart send')()
+    list_s_e = seq_s_e.split("\n")
+    pair_s_e = list_s_e[0]
+    start_end = pair_s_e.split(",")
+    dict_reads[(index,str(read.seq))] = start_end
+    break
+with open('test_case.txt', 'wb') as txt_file:
+    txt_file.write(pickle.dumps(dict_reads))
 
-result_handle.close()
-result_handle = open("my_blast.xml")
-blast_record = NCBIXML.read(result_handle)
-
-cmd = "blastx -query opuntia.fasta -db nr -out opuntia.xml"
-cmd += " -evalue 0.001 -outfmt 5"
-subprocess.run(cmd, shell=True)
-'''
-output = NcbiblastnCommandline(query=seq1, subject=seq2,
-                            evalue=0.001, remote=True, ungapped=True, outfmt=5)()[0]
-blast_result_record = NCBIXML.read(StringIO(output))
-
-for alignment in blast_result_record.alignments:
-    for hsp in alignment.hsps:
-        #if hsp.expect < 0.05:
-            print("****Alignment****")
-            print("sequence:", alignment.title)
-            print("length:", alignment.length)
-            print("e value:", hsp.expect)
-            print(hsp.query[0:75] + "...")
-            print(hsp.match[0:75] + "...")
-            print(hsp.sbjct[0:75] + "...")
+#find pairs of read that corresponds to same genomic region
+#code doesn't work right now
+dict_altered = dict_reads
+read_i = ""
+read_pairs = []
+for key,value in dict_reads.items():
+    search_region = dict_altered.pop(key)
+    read_i = key[1]
+    continue
+    if (value == search_region):
+        (i,j) = (read_i, key[1])
+        read_pairs.append(i,j)
