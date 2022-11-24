ffe34592a11f672345a82d5422ea6015b0c49a7a
Jessica <jessicazhu@MacBook-Pro.local>
2022-11-24 13:31:02 -0500

    get read pairs from same genomic region via blasting with genome seq

--- tester.py
+++ tester.py
@@ -22,36 +22,42 @@ seq_iterator = SeqIO.parse("genomic.fna.txt", "fasta")
 chromosome_1 = next(seq_iterator)
 
 #chromosome 1 sequence
-genome_seq = chromosome_1.seq
-chr1_seq = SeqRecord(genome_seq,
-                 id="chr1_seq")
-SeqIO.write(chr1_seq, "chr1_seq.fasta", "fasta")
-dict_reads = {}
+def generate_dict():
+    genome_seq = chromosome_1.seq
+    chr1_seq = SeqRecord(genome_seq,
+                     id="chr1_seq")
+    SeqIO.write(chr1_seq, "chr1_seq.fasta", "fasta")
+    dict_reads = {}
 
-#blast individual reads against chromosome 1 subject seq
-#generate dictionary where key has format (index, read sequence) and value of (genome region start, genome region end)
-for index,read in enumerate(SeqIO.parse("readsMappingToChr1.fa.txt", "fasta")):
-    seq1 = SeqRecord(Seq(read.seq),
-                     id="seq1")
-    SeqIO.write(seq1, "seq1.fasta", "fasta")
-    (seq_s_e,blank) = NcbiblastnCommandline(query="seq1.fasta", subject="seq2.fasta", outfmt='10 sstart send')()
-    list_s_e = seq_s_e.split("\n")
-    pair_s_e = list_s_e[0]
-    start_end = pair_s_e.split(",")
-    dict_reads[(index,str(read.seq))] = start_end
-    print(index)
-with open('test_case.txt', 'wb') as txt_file:
-    txt_file.write(pickle.dumps(dict_reads))
+    #blast individual reads against chromosome 1 subject seq
+    #generate dictionary where key has format (index, read sequence) and value of (genome region start, genome region end)
+    for index,read in enumerate(SeqIO.parse("readsMappingToChr1.fa.txt", "fasta")):
+        seq1 = SeqRecord(Seq(read.seq),
+                         id="seq1")
+        SeqIO.write(seq1, "seq1.fasta", "fasta")
+        (seq_s_e,blank) = NcbiblastnCommandline(query="seq1.fasta", subject="seq2.fasta", outfmt='10 sstart send')()
+        list_s_e = seq_s_e.split("\n")
+        pair_s_e = list_s_e[0]
+        start_end = pair_s_e.split(",")
+        dict_reads[(index,str(read.seq))] = start_end
+    with open('test_case.txt', 'wb') as txt_file:
+        txt_file.write(pickle.dumps(dict_reads))
 
 #find pairs of read that corresponds to same genomic region
-#code doesn't work right now
-"""dict_altered = dict_reads
+file = open("test_case.txt",'rb')
+dict_reads = pickle.load(file)
+#dict_altered = dict_reads
 read_i = ""
 read_pairs = []
+counter = 0
+
 for key,value in dict_reads.items():
-    search_region = dict_altered.pop(key)
-    read_i = key[1]
-    continue
-    if (value == search_region):
-        (i,j) = (read_i, key[1])
-        read_pairs.append(i,j)"""
+    for k,v in dict_reads.items():
+        search_region = value
+        read_i = key
+        if (v[0] == search_region[0] and v[1] == search_region[1]):
+            (i,j) = (read_i, k)
+            read_pairs.append((i,j))
+with open('true_pairs.txt', 'wb') as txt_file:
+    txt_file.write(pickle.dumps(read_pairs))
+file.close()
